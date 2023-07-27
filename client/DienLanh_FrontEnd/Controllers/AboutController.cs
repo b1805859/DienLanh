610969171229707ef0d511758f2247c2210e7e13
b1805859 <ngokinh30311@gmail.com>
2023-07-27 21:24:48 +0700

    add layout, view

---
+++ client/DienLanh_FrontEnd/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DienLanh_FrontEnd.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ client/DienLanh_FrontEnd/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DienLanh_FrontEnd.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
